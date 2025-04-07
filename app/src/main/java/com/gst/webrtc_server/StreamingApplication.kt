8afd60614d3798891c7f46d7c4bd156430698af4
floppyhammer <tannhauser_chen@outlook.com>
2025-04-07 10:30:40 +0800

    Fix handshake error

--- app/src/main/java/com/gst/webrtc_server/StreamingApplication.kt
+++ app/src/main/java/com/gst/webrtc_server/StreamingApplication.kt
@@ -11,13 +11,13 @@ import org.freedesktop.gstreamer.GStreamer
 class StreamingApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Log.i("GstWebrtcServer", "StreamingApplication: In onCreate")
-
-        System.loadLibrary("gstreamer_android")
-        Log.i("GstWebrtcServer", "StreamingApplication: loaded gstreamer_android")
-
-        Log.i("GstWebrtcServer", "StreamingApplication: Calling GStreamer.init")
-        GStreamer.init(this)
-        Log.i("GstWebrtcServer", "StreamingApplication: Done with GStreamer.init")
+//        Log.i("GstWebrtcServer", "StreamingApplication: In onCreate")
+//
+//        System.loadLibrary("gstreamer_android")
+//        Log.i("GstWebrtcServer", "StreamingApplication: loaded gstreamer_android")
+//
+//        Log.i("GstWebrtcServer", "StreamingApplication: Calling GStreamer.init")
+//        GStreamer.init(this)
+//        Log.i("GstWebrtcServer", "StreamingApplication: Done with GStreamer.init")
     }
 }
