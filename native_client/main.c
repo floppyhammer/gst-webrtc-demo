8b7059e934c738f388ed95a4fb0ea8fbf7b22e6e
floppyhammer <tannhauser_chen@outlook.com>
2025-05-11 09:56:39 +0800

    Fix regression caused by setting GST_TRACERS

--- native_client/main.c
+++ native_client/main.c
@@ -1,8 +1,5 @@
 #include "../src/client/client_pipeline.h"
-#include "../src/utils/platform.h"
 
 int main(int argc, char *argv[]) {
-    setenv("GST_TRACERS", "latency(flags=element)", 1);
-
     create_client(argc, argv);
 }
